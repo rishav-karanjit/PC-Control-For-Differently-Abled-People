28858230e4c44e9f7ff17b5418e27a80d44cae4d
unknown <karanjitrishav4@gmail.com>
2020-09-30 08:04:42 +0545

    Added News

---
+++ Classes/News.py
@@ -0,0 +1,23 @@
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+
+class News:
+    def GetTopNews(self):
+        news_url="https://news.google.com/news/rss"
+        Client=urlopen(news_url)
+        xml_page=Client.read()
+        Client.close()  
+        soup_page=soup(xml_page,"xml")
+        news_list=soup_page.findAll("item")
+        # Print news title, url and publish date
+        i=0
+        for news in news_list[:5]:
+            i +=1 
+            news = str(news.title.text)
+            news=news.replace("-", "\n\tNews by")
+            news = str(i) +" " + news + "\n"
+            print(news)
+
+N =News()
+N.GetTopNews()
