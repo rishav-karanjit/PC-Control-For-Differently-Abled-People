6b0e13be85b02f6b4d175061aaf366740ba04669
unknown <karanjitrishav4@gmail.com>
2020-09-30 09:06:44 +0545

    Changed dir of some file

--- DateAndTime.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-class DateAndTime:
-    def TimeNow(self):
-        return datetime.datetime.now().strftime("%H:%M:%S")
-
-    def DateNow(self):
-        return datetime.date.today()
--- Greeting.py
+++
@@ -1,3 +0,0 @@
-class GreetMe:
-    def Welcome(self):
-        return "Hello I am your virtual assistant. I will be helping you in controlling the PC. Do you have any commands for me"
