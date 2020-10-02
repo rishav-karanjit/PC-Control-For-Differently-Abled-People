f223eb12e269637f8d78433a9d466724ba64c1c4
unknown <karanjitrishav4@gmail.com>
2020-10-02 07:39:37 +0545

    Word control added

--- Controller.py
+++ Controller.py
@@ -8,6 +8,7 @@ from Classes.DateAndTime import DateAndTime
 from Classes.SpeechRecog import Command 
 from Classes.Greeting import GreetMe
 from Classes.SysInfo import GetSys
+from Classes.WordOp import Word
 
 class controller:
     def __init__(self):
@@ -49,8 +50,39 @@ class controller:
         elif "battery" in query:
             Ctr.Speak(SystemInfo.GetBattery())
 
+        elif "write" in query:
+            self.WordWCtr()
+
         else:
             Ctr.Speak("Say that again please")
+
+    def WordWCtr(self):
+        flag = 1
+        while(1):
+            if(flag):
+                Ctr.Speak("I can write a heading for you or a paragraph what do you want?")
+            else:
+                Ctr.Speak("I can your save or write one more heading or paragraph")
+
+            HorP = Com.TakeCommand().lower()
+            if "paragraph" in HorP:
+                Ctr.Speak("What do you want me to write?")
+                Write = Com.TakeCommand()
+                Word.WAddPara(Write)
+                Ctr.Speak("Paragraph written")
+                flag = 0
+
+            elif "save" in HorP:
+                Ctr.Speak("What shall I name the file")
+                FileName = Com.TakeCommand()
+                Word.SaveWrittenTxt(FileName)
+                return
+                
+            else:
+                Ctr.Speak("What do you want me to write?")
+                Write = Com.TakeCommand()
+                flag = 0
+                Ctr.Speak("Heading written.")
 
 Ctr = controller()
 Greet = GreetMe()
@@ -62,4 +94,5 @@ DT = DateAndTime()
 S = Search()
 N = News()
 SystemInfo = GetSys()
+Word = Word()
 Ctr.option(query)
